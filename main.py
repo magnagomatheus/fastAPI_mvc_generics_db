d2308a5f16cc8d133aebfa12e725840531a66fa9
Matheus Magnago <magnagomatheus7@gmail.com>
2025-10-06 09:59:43 -0300

    Class Pessoa with BaseModel from pydantic and path operation .post to create a Pessoa class created

--- main.py
+++ main.py
@@ -1,4 +1,13 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
+
+
+# Classe pessoa herdando BaseModel from pydantic
+class Pessoa(BaseModel):
+    name:str
+    endereco:str
+    # None = None para deixar um atributo como opcional.
+    telefone:str | None = None
 
 app = FastAPI()
 
@@ -6,3 +15,13 @@ app = FastAPI()
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+
+
+@app.get("/cadastrar/{nome}/{endereco}")
+def cadastrar(nome:str, endereco:str):
+    return {"message": f"{nome} foi cadastrado com o endereco: {endereco}"}
+
+# creating a path operation .post declaring the class Pessoa as path/query parameter.
+@app.post("/cadastrarPessoa/")
+async def cadastrarPessoa(p:Pessoa):
+    return p
