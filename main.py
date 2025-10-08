47007de0954bb6dde3bceec565210b11824b5935
Matheus Magnago <magnagomatheus7@gmail.com>
2025-10-08 07:27:58 -0300

    Database connection and creation of data models and table models completed

--- main.py
+++ main.py
@@ -12,33 +12,63 @@ from typing import Optional, List, Annotated
 
 # Address has to be on top because the python read top to bottom...
 
-# Criating the Address class to relate to Person
-class Address(SQLModel, table=True):
-    address_id: int | None = Field(default=None, primary_key = True, index=True)
+
+class AddressBase(SQLModel):
     logradouro: str = Field(index=True)
     numero: int = Field(index=True)
     estado: str = Field(index=True)
     cidade: str = Field(index=True)
-    bairro: str = Field(index=True)
+    bairro: str = Field(index=True) 
 
+# Creating the Address class to relate to Person
+class Address(AddressBase, table=True):
+    address_id: int | None = Field(default=None, primary_key = True, index=True)
     #back_populates liga Address e Person
     person: Optional["Person"] = Relationship(back_populates="address")
 
+class AddressPublic(AddressBase):
+    address_id: int
+
+class AddressCreate(AddressBase):
+    pass
+
+class AddressUpdate(AddressBase):
+    logradouro: str | None = None
+    numero: int | None = None
+    estado: str | None = None
+    cidade: str | None = None
+    bairro: str | None = None
 
 # Criando a classe Pessoa para conexao com banco de dados, usando SQLModel
 # table = true --> Tells SQLModel that it should represent a table in the SQL database.
-class Person(SQLModel, table=True):
-    # Field(primary_key = True) tells that the ID is the primary key in the SQL database
-    person_id: int | None = Field(default=None, primary_key = True, index=True)
+
+class PersonBase(SQLModel):
     # Field(index = True) make SQLModel create a SQL index for this column (attribute)
     name: str = Field(index=True)
+    
 
+class Person(PersonBase, table=True):
+    # Field(primary_key = True) tells that the ID is the primary key in the SQL database
+    person_id: int | None = Field(default=None, primary_key = True, index=True)
+    
     #foreing key from address
     address_id: int = Field(foreign_key="address.address_id")
 
     # relation
     #back_populates liga Person e Address
     address: Optional[Address] = Relationship(back_populates="person")
+
+class PersonPublic(PersonBase):
+    person_id:int
+
+# Create a person bonded with a address
+class PersonCreate(PersonBase):
+    address_id: Optional[int] = None
+
+class PersonUpdate(PersonBase):
+    name: str | None = None
+
+
 
 
 ### SQLModel Classes creating
@@ -91,16 +121,17 @@ def on_startup():
 
 
 # Creating a Person
-@app.post("/create_person/")
-def create_person(person: Person, session: SessionDep) -> Person:
-    session.add(person)
+@app.post("/create_person/", response_model=PersonPublic)
+def create_person(person: PersonCreate, session: SessionDep):
+    db_person = Person.model_validate(person)
+    session.add(db_person)
     session.commit()
-    session.refresh(person)
-    return person
+    session.refresh(db_person)
+    return db_person
 
 
 # Read Persons from the database
-@app.get("/read_persons/")
+@app.get("/read_persons/", response_model=list[PersonPublic])
 def read_persons(
     session: SessionDep,
     offset: int = 0,
@@ -110,12 +141,25 @@ def read_persons(
     return persons
 
 # Read a Person from database by id
-@app.get("/read_person/{person_id}")
+@app.get("/read_person/{person_id}", response_model=PersonPublic)
 def read_person(person_id: int, session:SessionDep) -> Person:
     person = session.get(Person, person_id)
     if not person:
         raise HTTPException(status_code=404, detail="Person not found")
     return person
+
+# Update a Person
+@app.patch("/update_person/{person_id}", response_model=PersonPublic)
+def update_person(person_id:int, person: PersonUpdate, session:SessionDep):
+    db_person = session.get(Person, person_id)
+    if not db_person:
+        raise HTTPException(status_code=404, detail="Hero not found")
+    person_data = person.model_dump(exclude_unset=True)
+    db_person.sqlmodel_update(person_data)
+    session.add(db_person)
+    session.commit()
+    session.refresh(db_person)
+    return db_person
 
 
 # Delete Person from database by id
@@ -132,21 +176,37 @@ def delete_person(person_id: int, session:SessionDep):
 # ADDRESS
 
 # Creating a Address
-@app.post("/create_address/")
-def create_address(address: Address, session: SessionDep) -> Address:
-    session.add(address)
+@app.post("/create_address/", response_model=AddressPublic)
+def create_address(address: AddressCreate, session: SessionDep) -> AddressPublic:
+    db_address = Address.model_validate(address)
+    session.add(db_address)
     session.commit()
-    session.refresh(address)
-    return address
+    session.refresh(db_address)
+    return db_address
 
 
-# Read a Person from database by id
-@app.get("/read_address/{address_id}")
-def read_person(address_id: int, session:SessionDep) -> Address:
+
+# Read a Address from database by id
+@app.get("/read_address/{address_id}", response_model=AddressPublic)
+def read_address(address_id: int, session:SessionDep) -> Address:
     address = session.get(Address, address_id)
     if not address:
         raise HTTPException(status_code=404, detail="Address not found")
     return address
+
+
+# Update a Address
+@app.patch("/update_address/{address_id}", response_model=AddressPublic)
+def update_address(address_id:int, address:AddressUpdate, session:SessionDep):
+    db_address = session.get(Address, address_id)
+    if not db_address:
+        raise HTTPException(status_code=404, detail="Hero not found")
+    address_data = address.model_dump(exclude_unset=True)
+    db_address.sqlmodel_update(address_data)
+    session.add(db_address)
+    session.commit()
+    session.refresh(db_address)
+    return db_address
 
 
 # Delete address from the database
